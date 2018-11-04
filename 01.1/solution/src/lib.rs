88a10c2d44d73aa2de90a1d0624252f28700e5c2
Milen Donchev <milen_d@abv.bg>
2018-11-04 17:15:57 +0200

    Finish sample tests

--- 01.1/solution/src/lib.rs
+++ 01.1/solution/src/lib.rs
@@ -19,7 +19,31 @@ pub enum CodeStyleError {
 impl FromStr for CodeStyle {
     type Err = CodeStyleError;
 
-    fn from_str(_identifier: &str) -> Result<CodeStyle, CodeStyleError> {
+    fn from_str(identifier: &str) -> Result<CodeStyle, CodeStyleError> {
+        let identifier = identifier.trim();
+        if identifier == "" {
+            return Err(CodeStyleError::EmptyString);
+        }
+
+        let first_char = identifier.chars().next().unwrap();
+        if !first_char.is_alphabetic() {
+            return Err(CodeStyleError::InvalidFirstChar(first_char))
+        }
+
+        let mut contains_underscore = false;
+        let contains_lower = false;
+        let contains_upper = false;
+        for (_i, c) in identifier.chars().enumerate() {
+            if c == '_' {
+                contains_underscore = true;
+            }
+            else if !c.is_alphanumeric() {
+                return Err(CodeStyleError::InvalidChar(c));
+            }
+        }
+
+        println!("{}{}{}", contains_underscore, contains_lower, contains_upper);
+
         Ok(CodeStyle::Underscored)
     }
 }
--- 01.1/solution/tests/test_basic.rs
+++ 01.1/solution/tests/test_basic.rs
@@ -4,15 +4,89 @@ use std::str::FromStr;
 
 #[test]
 fn test_basic() {
-    let code_style = CodeStyle::from_str(" some_var ");
-    assert_eq!(code_style.unwrap(), CodeStyle::Underscored);
-
-    let code_style = CodeStyle::from_str("some_var");
-    assert_eq!(code_style.unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str(" some_var ").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("some_var").unwrap(), CodeStyle::Underscored);
 }
 
 #[test]
 fn test_basic_2() {
-    let code_style = "some_var".parse::<CodeStyle>();
-    assert_eq!(code_style.unwrap(), CodeStyle::Underscored);
+    assert_eq!("some_var".parse::<CodeStyle>().unwrap(), CodeStyle::Underscored);
+}
+
+#[test]
+fn test_validity() {
+    assert_eq!(CodeStyle::from_str("").unwrap_err(), CodeStyleError::EmptyString);
+    assert_eq!(CodeStyle::from_str("  ").unwrap_err(), CodeStyleError::EmptyString);
+    assert_eq!(CodeStyle::from_str("some-var").unwrap_err(), CodeStyleError::InvalidChar('-'));
+    assert_eq!(CodeStyle::from_str("some var").unwrap_err(), CodeStyleError::InvalidChar(' '));
+    assert_eq!(CodeStyle::from_str("+_+").unwrap_err(), CodeStyleError::InvalidFirstChar('+'));
+    assert_eq!(CodeStyle::from_str("❤️_❤️").unwrap_err(), CodeStyleError::InvalidFirstChar("❤️".chars().next().unwrap()));
+    assert_eq!(CodeStyle::from_str("123abc").unwrap_err(), CodeStyleError::InvalidFirstChar('1'));
+    assert_eq!(CodeStyle::from_str("a+b=c").unwrap_err(), CodeStyleError::InvalidChar('+'));
+
+    assert_eq!(CodeStyle::from_str("abc123").unwrap(), CodeStyle::Unknown);
+    assert_eq!(CodeStyle::from_str("some_var").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str(" some_var ").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("someVar").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("SomeVar").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("Some_Var_").unwrap(), CodeStyle::Mixed);
+    assert_eq!(CodeStyle::from_str("Some_Var").unwrap(), CodeStyle::Mixed);
+    assert_eq!(CodeStyle::from_str("someVar_").unwrap(), CodeStyle::Mixed);
+}
+
+#[test]
+fn test_screaming_snakecase_basic() {
+    assert_eq!(CodeStyle::from_str("SOME_VAR").unwrap(), CodeStyle::ScreamingSnakecased);
+    assert_eq!(CodeStyle::from_str("SOME__VAR____").unwrap(), CodeStyle::ScreamingSnakecased);
+}
+
+#[test]
+fn test_cyrillic1() {
+    assert_eq!(CodeStyle::from_str("що_стана").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("що___стана").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("ЩО_СТАНА").unwrap(), CodeStyle::ScreamingSnakecased);
+    assert_eq!(CodeStyle::from_str("ЩО___СТАНА").unwrap(), CodeStyle::ScreamingSnakecased);
+    assert_eq!(CodeStyle::from_str("щоСтана").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("ЩоСтана").unwrap(), CodeStyle::Camelcased);
+}
+
+#[test]
+fn test_multibyte_uppercase() {
+    assert_eq!(CodeStyle::from_str("someSSpecialCase").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("SOME_SSPECIAL_CASE").unwrap(), CodeStyle::ScreamingSnakecased);
+    assert_eq!(CodeStyle::from_str("SOME___SSPECIAL___CASE").unwrap(), CodeStyle::ScreamingSnakecased);
+}
+
+#[test]
+fn test_whitespace() {
+    assert_eq!(CodeStyle::from_str("  some_var\n").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("\tone_two_three  ").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("  some___var\n").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("\tone__two_three__  ").unwrap(), CodeStyle::Underscored);
+}
+
+#[test]
+fn test_digits1() {
+    assert_eq!(CodeStyle::from_str("someVar123").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("some_var_123").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("some123Var").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("some_123_var").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("some__var__123_").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("some_123__var__").unwrap(), CodeStyle::Underscored);
+}
+
+#[test]
+fn test_digits2() {
+    assert_eq!(CodeStyle::from_str("some12Var").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("some_1_2_var").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("because789").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("because_7_8_9").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("because_7_8_9____").unwrap(), CodeStyle::Underscored);
+}
+
+#[test]
+fn test_digits3() {
+    assert_eq!(CodeStyle::from_str("some٣Var").unwrap(), CodeStyle::Camelcased);
+    assert_eq!(CodeStyle::from_str("some_٣_var").unwrap(), CodeStyle::Underscored);
+    assert_eq!(CodeStyle::from_str("some_٣__var__").unwrap(), CodeStyle::Underscored);
 }
